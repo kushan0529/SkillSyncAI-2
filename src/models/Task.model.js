3543cd015b984062555612664ef125256b286661
kushan <kkushan090@gmail.com>
2026-01-11 22:44:37 +0530

    feat(v1.0): complete manual skills task CRUD

--- src/models/Task.model.js
+++ src/models/Task.model.js
@@ -1,5 +1,7 @@
 // const mongoose = require('mongoose');
 
+const { mongo } = require("mongoose");
+
 // const suggestionSchema = new mongoose.Schema({
 //   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 //   score: Number,
@@ -22,25 +24,15 @@
 // module.exports = mongoose.model('Task', taskSchema);
 
 
-const mongoose=require('mongoose')
-const { diffIndexes } = require('./Project.model')
 
-const suggestionSchema=new mongoose.Schema({
-  userid:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
-  score:Number,
-  reason:String,
-},{_id:false})
-
-const TaskSchema=new mongoose.Schema({
+//version -1
+const mongoose=require('mongoose');
+const taskSchema = new mongoose.Schema({
   title:{type:String,required:true},
   description:String,
-  project:{type:mongoose.Schema.Types.ObjectId,ref:'project'},
-  assignee:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
-  status:{type:String,enum:['todo','in-progress','done'],default:'todo'},
-  preference:{type:String,enum:['low','medium','high'],default:'high'},
-  estimatedHours:Number,
-  dueDate:Date,
-  aiSuggestions:[suggestionSchema]
+  skills:[{type:String}],
+  status:{type:String,enum:['open','assigned','done'],default:'open'},
+  assigne:{type:mongoose.Schema.Types.ObjectId,ref:'user'}
 },{timestamps:true})
 
-module.exports=mongoose.model('Task',TaskSchema)
+exports.Task=mongoose.model('Task',taskSchema)
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,20 @@
-const router = require('express').Router();
-const auth = require('../middleware/auth.middleware');
-const TaskController = require('../controllers/task.controller');
+// const router = require('express').Router();
+// const auth = require('../middleware/auth.middleware');
+// const TaskController = require('../controllers/task.controller');
 
-router.post('/', auth, TaskController.createTask);
-router.put('/:id', auth, TaskController.updateTask);
-router.get('/:id', auth, TaskController.getTask);
-router.get('/project/:projectId', auth, TaskController.listByProject);
+// router.post('/', auth, TaskController.createTask);
+// router.put('/:id', auth, TaskController.updateTask);
+// router.get('/:id', auth, TaskController.getTask);
+// router.get('/project/:projectId', auth, TaskController.listByProject);
 
-module.exports = router;
+// module.exports = router;
+
+
+//version-1
+const express=require('express');
+const router=express.Router();
+
+const {createTask,getTask}=require('../controllers/task.controller');
+
+router.post('/',createTask);
+router.get('/',getTask);

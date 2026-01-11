4b30e93754c281ba7d86b574a097aaf16a0cf442
kushan <kkushan090@gmail.com>
2026-01-11 22:05:13 +0530

    feat(v1): add manual skills task model and routes

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,34 +1,30 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password: { type: String },
-  role: { type: String, enum: ['admin', 'manager', 'member'], default: 'member' },
-  skills: [{ type: String }],
-  resumeUrl: { type: String },
-  availabilityScore: { type: Number, default: 1 }
-}, { timestamps: true });
+// const userSchema = new mongoose.Schema({
+//   name: { type: String, required: true, trim: true },
+//   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+//   password: { type: String },
+//   role: { type: String, enum: ['admin', 'manager', 'member'], default: 'member' },
+//   skills: [{ type: String }],
+//   resumeUrl: { type: String },
+//   availabilityScore: { type: Number, default: 1 }
+// }, { timestamps: true });
 
-userSchema.index({ email: 1 });
+// userSchema.index({ email: 1 });
 
-module.exports = mongoose.model('User', userSchema);
-
-
+// module.exports = mongoose.model('User', userSchema);
 
 
 
-// const mongoose=require('mongoose');
 
-// const userSchema=mongoose.Schema({
-//   name:{type:String,required:true,trim:true},
-//   email:{type:String,require:true,unique:true},
-//   password:{type:String},
-//   role:{type:String,enum:['admin','manager','member'],
-//   skills:[{type:String}],
-//   resumeurl:{type:String},
-//   availabilityScore:{type:Number,default:1}
-//   }
-// },{ timestamps:true})
 
-// module.exports=mongoose.model('User',userSchema)
+const mongoose=require('mongoose')
+const userSchema=new mongoose.Schema({
+  name:{type:String,required:true,trim:true},
+  email:{type:String,required:true,trim:true,unique:true,lowercase:true},
+  password:{type:String},
+  role:{type:String,enum:['admin','manager','member'],default:'member'},
+  skills:[{type:String}],
+  resumeUrl:{type:String},
+  availabilityScore:{type:Number,default:1}
+},{timestamps:true})
